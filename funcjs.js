a8662e4a788e8613e27dfdb310957be798e1ed3f
Avital Ben-Natan <avital.ben-natan@sap.com>
2016-04-07 22:33:14 +0300

    Added arrayInitDecorator and removed requestFullScreen methods

--- funcjs.js
+++ funcjs.js
@@ -162,10 +162,11 @@ function trim (str, delimiter) {
  */
 function setProperty(obj, value, path, delimiter) {
 
-    if(!!!obj) throw new Error("Cannot set property on null, Must pass an object in the 1st argument");
-    if(!!!value) throw new Error("Must pass a value in the 2nd argument");
+    if(!obj) throw new Error("Cannot set property on null, Must pass an object in the 1st argument");
+    if(arguments.length<3) throw new Error("First 3 parameters must be defined");
+    //if(!!!value) throw new Error("Must pass a value in the 2nd argument");
 
-    if(!!!arguments[2]){
+    if(!arguments[2]){
         obj = value;
         return obj;
     }
@@ -337,6 +338,31 @@ function clone(src) {
     }
     return mixin(r, src, clone);
 
+}
+
+/**
+ * This function returns a member function so must be called with a context containing the array in the sPathToArray path
+ * @param sPathToArray the path to array to be intiallized
+ * @param {string|string[]} keys the keys to initialized
+ * @param {value|value[]} values the initial values
+ * @param {fn} [optional] callback function called after intialize on the decorated context with the arguments for call on the returned function instance
+ * @returns {Function} the initilializing context dependent function
+ */
+
+function arrayInitDecorator(sPathToArray, keys, values, callback){
+    var args = {sPathToArray:sPathToArray, keys:(isArray(keys)? keys: [keys]), values: (isArray(values)? values: [values]), callback: callback};
+
+    // function that initializes the array in the given path with the corresponding keys and values
+    return function(){
+        getObjProperty(this, args.sPathToArray).forEach(function(arrayElement){
+            var arrElement = arrayElement;
+            args.keys.forEach(function(key,i){
+                setProperty(arrayElement, args.values[i], key);
+            });
+        });
+        if(args.callback) args.callback.apply(this, arguments);
+        return this;
+    }
 }
 
 function removeObjProperty(obj, path, delimiter){
@@ -1172,37 +1198,6 @@ function $waitUntil(check,onComplete,delay,timeout) {
 
 /*! https://github.com/alexei/sprintf.js sprintf-js | Alexandru Marasteanu <hello@alexei.ro> (http://alexei.ro/) | BSD-3-Clause */
 !function(a){function b(){console.log("https://github.com/alexei/sprintf.js"); var a=arguments[0],c=b.cache;return c[a]&&c.hasOwnProperty(a)||(c[a]=b.parse(a)),b.format.call(null,c[a],arguments)}function c(a){return Object.prototype.toString.call(a).slice(8,-1).toLowerCase()}function d(a,b){return Array(b+1).join(a)}var e={not_string:/[^s]/,number:/[diefg]/,json:/[j]/,not_json:/[^j]/,text:/^[^\x25]+/,modulo:/^\x25{2}/,placeholder:/^\x25(?:([1-9]\d*)\$|\(([^\)]+)\))?(\+)?(0|'[^$])?(-)?(\d+)?(?:\.(\d+))?([b-gijosuxX])/,key:/^([a-z_][a-z_\d]*)/i,key_access:/^\.([a-z_][a-z_\d]*)/i,index_access:/^\[(\d+)\]/,sign:/^[\+\-]/};b.format=function(a,f){var g,h,i,j,k,l,m,n=1,o=a.length,p="",q=[],r=!0,s="";for(h=0;o>h;h++)if(p=c(a[h]),"string"===p)q[q.length]=a[h];else if("array"===p){if(j=a[h],j[2])for(g=f[n],i=0;i<j[2].length;i++){if(!g.hasOwnProperty(j[2][i]))throw new Error(b("[sprintf] property '%s' does not exist",j[2][i]));g=g[j[2][i]]}else g=j[1]?f[j[1]]:f[n++];if("function"==c(g)&&(g=g()),e.not_string.test(j[8])&&e.not_json.test(j[8])&&"number"!=c(g)&&isNaN(g))throw new TypeError(b("[sprintf] expecting number but found %s",c(g)));switch(e.number.test(j[8])&&(r=g>=0),j[8]){case"b":g=g.toString(2);break;case"c":g=String.fromCharCode(g);break;case"d":case"i":g=parseInt(g,10);break;case"j":g=JSON.stringify(g,null,j[6]?parseInt(j[6]):0);break;case"e":g=j[7]?g.toExponential(j[7]):g.toExponential();break;case"f":g=j[7]?parseFloat(g).toFixed(j[7]):parseFloat(g);break;case"g":g=j[7]?parseFloat(g).toPrecision(j[7]):parseFloat(g);break;case"o":g=g.toString(8);break;case"s":g=(g=String(g))&&j[7]?g.substring(0,j[7]):g;break;case"u":g>>>=0;break;case"x":g=g.toString(16);break;case"X":g=g.toString(16).toUpperCase()}e.json.test(j[8])?q[q.length]=g:(!e.number.test(j[8])||r&&!j[3]?s="":(s=r?"+":"-",g=g.toString().replace(e.sign,"")),l=j[4]?"0"===j[4]?"0":j[4].charAt(1):" ",m=j[6]-(s+g).length,k=j[6]&&m>0?d(l,m):"",q[q.length]=j[5]?s+g+k:"0"===l?s+k+g:k+s+g)}return q.join("")},b.cache={},b.parse=function(a){for(var b=a,c=[],d=[],f=0;b;){if(null!==(c=e.text.exec(b)))d[d.length]=c[0];else if(null!==(c=e.modulo.exec(b)))d[d.length]="%";else{if(null===(c=e.placeholder.exec(b)))throw new SyntaxError("[sprintf] unexpected placeholder");if(c[2]){f|=1;var g=[],h=c[2],i=[];if(null===(i=e.key.exec(h)))throw new SyntaxError("[sprintf] failed to parse named argument key");for(g[g.length]=i[1];""!==(h=h.substring(i[0].length));)if(null!==(i=e.key_access.exec(h)))g[g.length]=i[1];else{if(null===(i=e.index_access.exec(h)))throw new SyntaxError("[sprintf] failed to parse named argument key");g[g.length]=i[1]}c[2]=g}else f|=2;if(3===f)throw new Error("[sprintf] mixing positional and named placeholders is not (yet) supported");d[d.length]=c}b=b.substring(c[0].length)}return d};var f=function(a,c,d){return d=(c||[]).slice(0),d.splice(0,0,a),b.apply(null,d)};"undefined"!=typeof exports?(exports.sprintf=b,exports.vsprintf=f):(a.sprintf=b,a.vsprintf=f,"function"==typeof define&&define.amd&&define(function(){return{sprintf:b,vsprintf:f}}))}("undefined"==typeof window?this:window);
-
-var requestFullscreen = function (ele) {
-    if(!ele) ele = document.documentElement;
-    if (ele.requestFullscreen) {
-        ele.requestFullscreen();
-    } else if (ele.webkitRequestFullscreen) {
-        ele.webkitRequestFullscreen();
-    } else if (ele.mozRequestFullScreen) {
-        ele.mozRequestFullScreen();
-    } else if (ele.msRequestFullscreen) {
-        ele.msRequestFullscreen();
-    } else {
-        // Fallback
-        console.log('Fullscreen API is not supported.');
-    }
-};
-
-var exitFullscreen = function () {
-    if (document.exitFullscreen) {
-        document.exitFullscreen();
-    } else if (document.webkitExitFullscreen) {
-        document.webkitExitFullscreen();
-    } else if (document.mozCancelFullScreen) {
-        document.mozCancelFullScreen();
-    } else if (document.msExitFullscreen) {
-        document.msExitFullscreen();
-    } else {
-        // Fallback
-        console.log('Fullscreen API is not supported.');
-    }
-};
 
 //************************** Type definitions ******************************//
 // definition of validation error
